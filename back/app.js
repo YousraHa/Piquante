537f6c6ce6f69e19bb392662ec74c8e5f4dd5433
Yousra H <yousrah02@hotmail.com>
2023-01-06 18:31:53 +0100

    adding app.js

--- back/app.js
+++ back/app.js
@@ -1,66 +1,33 @@
 const express = require('express');
+const bodyParser = require("body-parser")
 const mongoose = require('mongoose');
-const stuffRoutes = require('./routes/test');
+const path = require('path');
+
+const sauceRoute = require('./routes/sauce');
+const userRoute = require('./routes/user');
+const User = require('./models/user');
+const Sauce = require('./models/sauce');
 
 const app = express();
+app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://opcpiquante:GNMeLvQohXemmmho@cluster0.au1uftb.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://opcpiquante:Lbch6R87WhoqAQdO@cluster0.au1uftb.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  app.use('/', stuffRoutes);
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
   
-// app.get('/', (req, res, next) => {
-//   res.send('accueil!');
-// });
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoute);
+// app.use('/api/user', userRoute);
+app.use('/api/auth', userRoute);
 
-// app.get('/api/sauces', (req, res, next) => {
-//   res.send('api sauces GET!');
-// });
-
-// app.post('/api/sauces', (req, res, next) => {
-//   res.send('api sauces POST!');
-// });
-
-// app.get('/api/sauces/:id', (req, res, next) => {
-//   res.json({message: 'api ID get!'});
-// });
-
-// app.put('/api/sauces/:id', (req, res, next) => {
-//   res.json({message: 'api ID put!'});
-// });
-
-// app.delete('/api/sauces/:id', (req, res, next) => {
-//   res.json({message: 'sauce delete!'});
-// });
-
-// app.post('/api/sauces/:id/like', (req, res, next) => {
-//   res.json({message: 'id like!'});
-// });
-
-
-// app.get('/api/opc', (req, res, next) => {
-//   const stuff = [
-//     {
-//       _id: 'oeihfzeoi',
-//       title: 'Mon premier objet',
-//       description: 'Les infos de mon premier objet',
-//       imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-//       price: 4900,
-//       userId: 'qsomihvqios',
-//     },
-//     {
-//       _id: 'oeihfzeomoihi',
-//       title: 'Mon deuxième objet',
-//       description: 'Les infos de mon deuxième objet',
-//       imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-//       price: 2900,
-//       userId: 'qsomihvqios',
-//     },
-//   ];
-//   res.status(200).json(stuff);
-// });
 
 module.exports = app;
