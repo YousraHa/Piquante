f26660db8bc71fd9dd805f171c600b8345ce6b6b
Yousra H <yousrah02@hotmail.com>
2023-01-18 16:53:00 +0100

    fix heat type and unique user

--- back/models/user.js
+++ back/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
